14c143199ec11b42d189c89432745fd47454c709
mytime <qushe@me.com>
2016-09-22 20:01:48 +0800

    switch流程控制

---
+++ Swift基本数据类型/switch循环/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  switch循环
+//
+//  Created by wanfu yan on 16/9/22.
+//  Copyright © 2016年 yanwanfu. All rights reserved.
+//
+
+import Foundation
+
+let score = 99;
+
+switch(score/10)
+{
+case 9:
+    print("优秀")
+case 8:
+    print("良好")
+case 7:
+    print("不错")
+case 6:
+    print("及格")
+default:
+    print("不及格")
+}
+
