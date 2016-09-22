78aaee9248d4c7f986d793cbb75c15531ea06e5e
mytime <qushe@me.com>
2016-09-22 19:52:53 +0800

    if循环

--- Swift基本数据类型/Swift流程控制/main.swift
+++ Swift基本数据类型/Swift流程控制/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
+
+let score = -1
+
+if score>100 || score < 0
+{
+    print("非法数字")
+}else if score >= 90
+{
+    print("成绩优秀")
+}else if score >= 80
+{
+    print("成绩良好")
+}else if score >= 70
+{
+    print("还可以")
+}else if score >= 60
+{
+    print("及格")
+}else
+{
+    print("不及格")
+}
+
 print("Hello, World!")
 
