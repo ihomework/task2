6e792c1641698535a30ba4d209607db3a465d03a
mytime <cmba@qq.com>
2016-09-23 08:00:32 +0800

    swift字符串与object-C字符串互转

---
+++ Swift基本数据类型/15.字符串类型/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  15.字符串类型
+//
+//  Created by iwan on 16/9/23.
+//  Copyright © 2016年 yanwanfu. All rights reserved.
+//
+
+import Foundation
+
+// swift 字符串
+let s = "yanwanfu"
+print(s)
+
+// Object-C 字符串
+let s1:NSString = "NSString"
+print(s1)
+
+// 把Object-C字符串转换为swift字符串，使用as关键字
+let s2 = s1 as String
+print(s2)
+
+// 把swift字符串转换为Object-C字符串，使用as关键字
+let s3 = s as NSString
+print(s3)
+
