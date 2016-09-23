574889b120bbd67b83df836b917301603a1978f9
mytime <cmba@qq.com>
2016-09-23 09:26:51 +0800

    emoji使用

---
+++ Swift基本数据类型/19.认识emoji/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  19.认识emoji
+//
+//  Created by iwan on 16/9/23.
+//  Copyright © 2016年 yanwanfu. All rights reserved.
+//
+
+import Foundation
+/*
+ emoji： 是一套表情
+ 
+ Edit -> Emoji - > 插入表情
+ 
+ 
+ */
+
+var s = "💍🏎"
+print(s)
+
